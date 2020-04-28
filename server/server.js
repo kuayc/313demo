f05d153a9cc4f1f18cc86811ebc072926c92f005
Chin Kuay <kuaychin@gmail.com>
2020-04-28 11:28:36 -0500

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
-// Setup express app
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(
     bodyParser.urlencoded({
         extended: false
     })
 );
-
 app.use(bodyParser.json());
 
 // Configure Mongo
@@ -25,6 +22,5 @@ mongoose
     .then(() => console.log("Mongo connected"))
     .catch(err => console.log(err));
 
-// Specify the Port where the backend server can be accessed and start listening on that port
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server up and running on port ${port}.`));
+app.listen(port, () => console.log(`Server port ${port}.`));
